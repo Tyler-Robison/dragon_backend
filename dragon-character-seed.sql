f6c23572c0c81fc09759e55270d2d4b3e442efd6
Tyler Robison <tylerobison758758@gmail.com>
2022-08-05 13:23:28 -0400

    added exp to characters

--- dragon-character-seed.sql
+++ dragon-character-seed.sql
@@ -1,6 +1,6 @@
 INSERT INTO characters (name, race, class, Level, AC, STR, STRMod, DEX, DEXMod, CON, CONMod, INT, INTMod, WIS, WISMod, CHA, CHAMod, HP, speed, user_id)
-VALUES ('Tyler Robison', 'Human', 'Fighter', 1, 17, 16, 3, 12, 1, 14, 2, 7, -2, 10, 0, 9, -1, 22, 3, 1), 
-       ('Will Robison', 'Elf', 'Wizard', 1, 14, 9, -1, 12, 1, 12, 1, 18, 4, 12, 1, 11, 0, 16, 3, 1),
-       ('Wayne Robison', 'Dwarf', 'Warlock', 1, 16, 14, 2, 10, 0, 12, 1, 8, -1, 12, 1, 17, 3, 17, 3, 1),
-       ('Lindsey Robison', 'Elf', 'Cleric', 1, 15, 12, 1, 9, -1, 12, 1, 10, 0, 18, 4, 12, 1, 17, 3, 1),
-       ('Ralph Robison', 'Tabaxi', 'Rogue', 1, 16, 12, 1, 18, 4, 14, 2, 12, 1, 5, -3, 12, 1, 16, 3, 1);
+VALUES ('Tyler Robison', 'Human', 'Fighter', 1, 17, 16, 3, 12, 1, 14, 2, 7, -2, 10, 0, 9, -1, 22, 3, 0, 1), 
+       ('Will Robison', 'Elf', 'Wizard', 1, 14, 9, -1, 12, 1, 12, 1, 18, 4, 12, 1, 11, 0, 16, 3, 0, 1),
+       ('Wayne Robison', 'Dwarf', 'Warlock', 1, 16, 14, 2, 10, 0, 12, 1, 8, -1, 12, 1, 17, 3, 17, 3, 0, 1),
+       ('Lindsey Robison', 'Elf', 'Cleric', 1, 15, 12, 1, 9, -1, 12, 1, 10, 0, 18, 4, 12, 1, 17, 3, 0, 1),
+       ('Ralph Robison', 'Tabaxi', 'Rogue', 1, 16, 12, 1, 18, 4, 14, 2, 12, 1, 5, -3, 12, 1, 16, 3, 0, 1);
